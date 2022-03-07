8e735381c250c7277c144cb46c5a7699dfae5d05
飞鸟 <965555169@qq.com>
2022-03-07 19:15:57 +0800

    build: add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import s from './App.scss';
 import Test from '@/components/Test';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 const App = () => {
   return (
-    <div className={s.AppBox}>
-      <Test text='My React' />
-    </div>
+    <ErrorBoundary>
+      <div className={s.AppBox}>
+        <Test text='My React' />
+      </div>
+    </ErrorBoundary>
   );
 };
 
