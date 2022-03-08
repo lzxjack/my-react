603072bd35951bd959010b970b9917f3439353b4
飞鸟 <965555169@qq.com>
2022-03-08 18:14:22 +0800

    feat: add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
 import App from './App';
 
 if (module && module.hot) {
@@ -8,8 +10,10 @@ if (module && module.hot) {
 }
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import { DatePicker } from 'antd';
+import { DatePicker, Button, Space } from 'antd';
 import { useTitle } from 'ahooks';
+import { connect } from 'react-redux';
+import { setCount } from '@/redux/actions';
+import { storeState } from '@/redux/interface';
 
-const Home: React.FC = () => {
+interface Props {
+  count?: number;
+  setCount?: Function;
+}
+
+const Home: React.FC<Props> = ({ count, setCount }) => {
   useTitle('Home');
-  return <DatePicker />;
+
+  const add = () => {
+    setCount && setCount(count! + 1);
+  };
+
+  const sub = () => {
+    setCount && setCount(count! - 1);
+  };
+
+  return (
+    <div>
+      <DatePicker />
+      <div>
+        <h2>{count}</h2>
+        <Space>
+          <Button type='primary' onClick={add}>
+            ADD
+          </Button>
+          <Button type='primary' onClick={sub}>
+            SUB
+          </Button>
+        </Space>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default connect(
+  (state: storeState) => ({
+    count: state.count,
+  }),
+  { setCount }
+)(Home);
